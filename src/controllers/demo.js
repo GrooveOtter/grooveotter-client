cf80455c21693fa2ac9454e6560c5f95484110fb
Tenor Biel <tenorbiel@gmail.com>
2015-04-07 01:42:34 -0500

    implement countdown feature with time input for demo

--- src/controllers/demo.js
+++ src/controllers/demo.js
@@ -1,9 +1,23 @@
 angular.module('gotr')
     .controller('DemoController', DemoController);
 
-DemoController.$inject = ['clock'];
-function DemoController(clock) {
+DemoController.$inject = ['$scope', 'clock'];
+function DemoController($scope, clock) {
     var vm = this;
 
+    vm.duration = -1000;
     vm.clock = clock;
+    vm.start = start;
+
+    $scope.$watch('vm.clock.elapsedTime', function(time) {
+        if (time >= vm.duration) {
+            clock.stop();
+        }
+    });
+
+    function start() {
+        vm.duration = 1000 * vm.inputSeconds;
+        clock.reset();
+        clock.start();
+    }
 }
