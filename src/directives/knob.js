80053d6148d10db051ad0db5ed6a3250866d8c6d
Tenor Biel <tenorbiel@gmail.com>
2015-04-06 23:04:26 -0500

    add knob directive

---
+++ src/directives/knob.js
@@ -0,0 +1,95 @@
+angular.module('gotr')
+    .directive('gotrKnob', Knob);
+
+Knob.$inject = [];
+function Knob() {
+    var directive = {
+        restrit: 'EA',
+        replace: true,
+        templateUrl: 'knob.html',
+        scope: {
+            value: '=',
+            max: '=',
+            width: '=',
+            label: '='
+        },
+        controller: KnobController,
+        controllerAs: 'vm',
+        link: link,
+        bindToController: true
+    };
+
+    return directive;
+
+    function link(scope, element, _, vm) {
+        var canvas = angular.element(element[0]).find('canvas')[0];
+        var ctx = canvas.getContext('2d');
+
+        // There might be some shorthand to make this shorter
+        draw(vm.value, vm.max);
+
+        scope.$watch('vm.value', function(value) {
+            draw(value, vm.max);
+        });
+
+        scope.$watch('vm.max', function(max) {
+            draw(vm.value, max);
+        });
+
+        function draw(value, max) {
+            var min = 0;
+            var arc = 2 * Math.PI;
+            var center = vm.width / 2;
+            var bgLineWidth = center * vm.bgThickness;
+            var fgLineWidth = center * vm.fgThickness;
+            var isMax = max === value;
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            var angle = (value - min) * arc / (max - min);
+
+            var radius = center - bgLineWidth / 2;
+
+            var start = 1.5 * Math.PI;
+            var end = 1.5 * Math.PI + arc;
+
+            ctx.beginPath();
+            ctx.strokeStyle = vm.bgColor;
+            ctx.lineWidth = bgLineWidth;
+            ctx.arc(center, center, radius, end, start, true);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.strokeStyle = vm.fgColor;
+            ctx.lineWidth = fgLineWidth;
+            ctx.arc(center, center, radius, start, start + angle, !isMax);
+            ctx.stroke();
+        }
+    }
+}
+
+KnobController.$inject = [];
+function KnobController() {
+    var vm = this;
+
+    var width = vm.width;
+    vm.fgThickness = 0.03;
+    vm.bgThickness = 0.24;
+    vm.fgColor = '#77A78D';
+    vm.bgColor = '#EDEDED';
+
+    vm.inputStyle = {
+        position: 'absolute',
+        top: ((width / 2) - (width / 8)) + 'px',
+        left: (width / 2) * vm.bgThickness + 'px',
+        width: width - (width * vm.bgThickness) + 'px',
+        verticalAlign: 'middle',
+        border: '0px',
+        background: 'none',
+        font: '300 34px Muli',
+        textAlign: 'center',
+        color: '#4d4e5e',
+        padding: '0px',
+        WebkitAppearance: 'none'
+    };
+}
