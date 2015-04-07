2f42be458f4f4224102ee7e069d739757bde8447
Tenor Biel <tenorbiel@gmail.com>
2015-04-07 01:52:17 -0500

    improve demo UI features

--- src/controllers/demo.js
+++ src/controllers/demo.js
@@ -5,9 +5,12 @@ DemoController.$inject = ['$scope', 'clock'];
 function DemoController($scope, clock) {
     var vm = this;
 
+    vm.inputSeconds = 0;
     vm.duration = -1000;
     vm.clock = clock;
     vm.start = start;
+    vm.started = false;
+    vm.timeLeft = timeLeft;
 
     $scope.$watch('vm.clock.elapsedTime', function(time) {
         if (time >= vm.duration) {
@@ -19,5 +22,10 @@ function DemoController($scope, clock) {
         vm.duration = 1000 * vm.inputSeconds;
         clock.reset();
         clock.start();
+        vm.started = true;
+    }
+
+    function timeLeft() {
+        return vm.duration - vm.clock.elapsedTime + 1000;
     }
 }
