338cbf309748ae6b6e2eb749d564b1a646a3454b
Tenor Biel <tenorbiel@gmail.com>
2015-04-10 20:37:13 -0500

    track page load and unload times in local storage

--- src/config/run.js
+++ src/config/run.js
@@ -1,9 +1,17 @@
 angular.module('gotr')
     .run(Run);
 
-Run.$inject = ['$route', '$rootScope'];
-function Run($route, $rootScope) {
+Run.$inject = ['$route', '$rootScope', 'tracker'];
+function Run($route, $rootScope, tracker) {
     $rootScope.title = 'GrooveOtter';
+    tracker.start();
+
+    angular.element(window).on('unload', function() {
+        var time = localStorage.getItem('item') || 0;
+
+        tracker.stop();
+        localStorage.setItem('time', time + tracker.elapsedTime);
+    });
 
     $rootScope.$on('$routeChangeSuccess', function() {
         $rootScope.title = $route.current.title;
