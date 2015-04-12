108682db3fea80dd01985dcf95cad03cce7a00ee
Tenor Biel <tenorbiel@gmail.com>
2015-04-11 19:12:04 -0500

    document controllers

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,3 +1,11 @@
+/**
+ * This is an empty controller to eventually be used
+ * for enabling system navigation and hosting other
+ * controllers.
+ *
+ * @ngdoc controller
+ * @name MainController
+ */
 angular.module('gotr')
     .controller('MainController', Main);
 
