a972cc2ac9dc2e529f63ceda70a5be9b81116832
Tenor Biel <tenorbiel@gmail.com>
2015-04-12 09:24:04 -0500

    add AFK detection system

--- src/config/run.js
+++ src/config/run.js
@@ -1,8 +1,8 @@
 angular.module('gotr')
     .run(Run);
 
-Run.$inject = ['$route', '$rootScope', 'tracker', 'trackerStore'];
-function Run($route, $rootScope, tracker, trackerStore) {
+Run.$inject = ['$route', '$rootScope', 'tracker', 'trackerStore', 'debounce'];
+function Run($route, $rootScope, tracker, trackerStore, debounce) {
     $rootScope.title = 'GrooveOtter';
     tracker.start();
 
@@ -20,6 +20,11 @@ function Run($route, $rootScope, tracker, trackerStore) {
     angular.element(window).on('blur', function() {
         tracker.stop();
     });
+
+    angular.element(window).on('mousemove mousedown', debounce(function() {
+        tracker.stop();
+        tracker.elapsedTime -= 3 * 60 * 1000;
+    }, 3 * 60 * 1000));
 
     $rootScope.$on('$routeChangeSuccess', function() {
         $rootScope.title = $route.current.title;
