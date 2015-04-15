9cb323611fecdd7ca8367d869bab013726060194
Tenor Biel <tenorbiel@gmail.com>
2015-04-15 18:07:23 -0500

    extract dashboard into session directive
    This might be the wrong to do this...

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,45 +1,5 @@
 angular.module('gotr')
     .controller('DashboardController', DashboardController);
 
-DashboardController.$inject = ['Session'];
-function DashboardController(Session) {
-    /**
-     * @namespace
-     * @alias DashboardController
-     */
-    var vm = this;
-
-    /** The current session */
-    vm.session = null;
-
-    /** The choice of time limit in minutes */
-    vm.choice = 15;
-
-    /** Indicates if the user is selecting a time */
-    vm.selecting = false;
-
-    vm.start = start;
-    vm.isStarted = isStarted;
-    vm.complete = complete;
-
-    /**
-     * Creates the session and starts it
-     */
-    function start() {
-        vm.session = new Session(vm.taskName, vm.choice * 60 * 1000);
-        vm.session.start();
-    }
-
-    /**
-     * Indicates whether the user has initiated the session
-     * @memberof DashboardController
-     */
-    function isStarted() {
-        return vm.session != null;
-    }
-
-    function complete() {
-        vm.session.complete();
-        vm.session = null;
-    }
-}
+DashboardController.$inject = [];
+function DashboardController() {}
---
+++ src/directives/session.js
@@ -0,0 +1,65 @@
+angular.module('gotr')
+    .directive('gotrSession', SessionDirective);
+
+SessionDirective.$inject = [];
+function SessionDirective() {
+    var directive = {
+        restrit: 'EA',
+        replace: true,
+        templateUrl: 'session.html',
+        scope: {},
+        controller: SessionController,
+        controllerAs: 'vm',
+        bindToController: true
+    };
+
+    return directive;
+}
+
+SessionController.$inject = ['Session'];
+function SessionController(Session) {
+    /**
+     * @namespace
+     * @alias SessionController
+     */
+    var vm = this;
+
+    /** The current session */
+    vm.session = null;
+
+    /** The choice of time limit in minutes */
+    vm.choice = 15;
+
+    /** Indicates if the user is selecting a time */
+    vm.selecting = false;
+
+    vm.start = start;
+    vm.isStarted = isStarted;
+    vm.complete = complete;
+
+    /**
+     * Creates the session and starts it
+     * @memberof SessionController
+     */
+    function start() {
+        vm.session = new Session(vm.taskName, vm.choice * 60 * 1000);
+        vm.session.start();
+    }
+
+    /**
+     * Indicates whether the user has initiated the session
+     * @memberof SessionController
+     */
+    function isStarted() {
+        return vm.session != null;
+    }
+
+    /**
+     * Completes the session
+     * @memberof SessionController
+     */
+    function complete() {
+        vm.session.complete();
+        vm.session = null;
+    }
+}
