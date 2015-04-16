b1359c77241203d695a59dd8a960e724d0d4b1c7
Tenor Biel <tenorbiel@gmail.com>
2015-04-16 15:21:26 -0500

    configure dashboard controller to load task list

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,8 +6,16 @@ function Router($routeProvider, $locationProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'dashboard.html',
-            controller: 'DashboardController as vm'
+            controller: 'DashboardController as vm',
+            resolve: {
+                taskList: TaskList
+            }
         });
 
     $locationProvider.html5Mode(true);
+}
+
+TaskList.$inject = ['taskListStore'];
+function TaskList(taskListStore) {
+    return taskListStore.get();
 }
--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,5 +1,9 @@
 angular.module('gotr')
     .controller('DashboardController', DashboardController);
 
-DashboardController.$inject = [];
-function DashboardController() {}
+DashboardController.$inject = ['taskList'];
+function DashboardController(taskList) {
+    var vm = this;
+
+    this.taskList = taskList;
+}
