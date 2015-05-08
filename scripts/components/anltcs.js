0e2445e3a6c1c853bb652e28679ae493b0363290
Tenor Biel <tenorbiel@gmail.com>
2015-05-08 15:02:07 -0500

    render anltcs store data in anltcs component

--- scripts/components/anltcs.js
+++ scripts/components/anltcs.js
@@ -1,20 +1,52 @@
 var React = require('react');
+var Fluxxor = require('fluxxor');
+
+var FluxMixin = Fluxxor.FluxMixin(React);
+var StoreWatchMixin = Fluxxor.StoreWatchMixin;
 
 var Anltcs = module.exports = exports = React.createClass({
+    mixins: [FluxMixin, StoreWatchMixin('TaskListStore')],
+
+    getStateFromFlux: function() {
+        var flux = this.getFlux();
+        var AnltcsStore = flux.store('AnltcsStore');
+
+        return {
+            sessionTime: AnltcsStore.getSessionTime(),
+            siteTime: AnltcsStore.getSiteTime()
+        };
+    },
+
     render: function() {
+        var sessionTime = this.state.sessionTime;
+        var siteTime = this.state.siteTime;
+
+        var cord = (sessionTime / siteTime) * 130;
+        var working = Math.floor(sessionTime / (60 * 1000));
+        var organizing = Math.floor(siteTime / (60 * 1000)) - working;
+
+        // TODO: setup linguistics
+
         return <div className="gotr-anltcs">
-            <div className="gotr-anltcs-count">45</div>
+            <div className="gotr-anltcs-count">{working}</div>
             <div className="gotr-anltcs-desc">minutes in Groove</div>
             <svg className="gotr-anltcs-graphic">
                 <mask id="gotr-anltcs-bar">
-                    <line x1="10" y1="10" y2="10" x2="140"></line>
+                    <line x1="10" y1="10" y2="10" x2="140"/>
                 </mask>
-                <line x1="91" y1="10" x2="200" y2="10" className="gotr-anltcs-background"></line>
-                <line x1="0" y1="10" y2="10" x2="90" className="gotr-anltcs-foreground"></line>
+                <line x1={cord + 1} y1="10" x2="200" y2="10" className="gotr-anltcs-background"/>
+                <line x1="0" y1="10" y2="10" x2={cord} className="gotr-anltcs-foreground"/>
             </svg>
-            <div className="gotr-anltcs-minutes"><span className="gotr-anltcs-minutes-working"></span>577 working</div>
-            <div
-            className="gotr-anltcs-minutes"><span className="gotr-anltcs-minutes-organizing"></span>62 organizing</div>
+
+            <div className="gotr-anltcs-minutes">
+                <span className="gotr-anltcs-minutes-working"/>
+                {working} working
+            </div>
+
+            <div className="gotr-anltcs-minutes">
+                <span className="gotr-anltcs-minutes-organizing"/>
+                {organizing} organizing
+            </div>
         </div>;
     }
 });
