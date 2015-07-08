9410be18b54ac1b4d3b797c0f19851691d11f6fb
Garreth Dottin <gdottin@liveintent.com>
2015-07-08 09:07:29 -0400

    Removed analytics graph

--- scripts/components/anltcs.js
+++ scripts/components/anltcs.js
@@ -54,8 +54,6 @@ var Anltcs = module.exports = exports = React.createClass({
 
         var cord = Math.floor(sessionTime / siteTime * 100);
         var total = Math.floor(siteTime / (60 * 1000));
-        var working = Math.floor(sessionTime / (60 * 1000));
-        var organizing = total - working;
 
         // this occurs on page-load,
         // when sessionTime and siteTime are 0
@@ -67,27 +65,7 @@ var Anltcs = module.exports = exports = React.createClass({
         return <div className="gotr-anltcs">
             <div className="gotr-anltcs-count">{total}</div>
             <div className="gotr-anltcs-desc">minutes in Groove Today</div>
-            <div className="gotr-anltcs-graphic-container">
-                <svg className="gotr-anltcs-graphic">
-                    <mask id="gotr-anltcs-bar">
-                        <line x1="5" y1="5" y2="5" x2="100%"/>
-                    </mask>
 
-                    <line x1="0" y1="5" x2="200%" y2="5" className="gotr-anltcs-background"/>
-                    <line x1="0" y1="5" y2="5" x2={cord + '%'} className="gotr-anltcs-foreground"/>
-                    <line x1={cord + '%'} y1="0" x2={cord + '%'} y2="10" className="gotr-anltcs-divider"/>
-                </svg>
-            </div>
-
-            <div className="gotr-anltcs-minutes">
-                <span className="gotr-anltcs-minutes-working"/>
-                {working} working
-            </div>
-
-            <div className="gotr-anltcs-minutes">
-                <span className="gotr-anltcs-minutes-organizing"/>
-                {organizing} organizing
-            </div>
         </div>;
     }
 });
