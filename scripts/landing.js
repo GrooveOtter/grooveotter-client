f7660227748d0cf99624cc30db14e2bbbdc5018e
Chris Lewis <chrislewis@golfingbuddha.com>
2016-03-18 21:41:25 -0400

    new homepage

--- scripts/landing.js
+++ scripts/landing.js
@@ -7,75 +7,75 @@ if (cookies.get('loggedin')) {
     window.location = '/app';
 }
 
-$(function() {
-    if ($(window).width() > 800) {
-        var video = $('.gotrl-panel-three-right video').scrolling({
-            offsetLeft: 700,
-            offsetTop: -100
-        });
-
-        var anltcs = $('.gotrl-panel-four-left img').scrolling({
-            offsetLeft: -700,
-            offsetTop: -30
-        });
-
-        var social = $('.gotrl-panel-social-right img').scrolling({
-            offsetLeft: 700,
-            offsetTop: -100
-        });
-
-
-        if (video.is(':scrollin')) {
-            slideInFromRight.call(video);
-        } else {
-            video.one('scrollin', slideInFromRight);
-        }
-
-        if (anltcs.is(':scrollin')) {
-            slideInFromLeft.call(anltcs);
-        } else {
-            anltcs.one('scrollin', slideInFromLeft);
-        }
-
-        if (social.is(':scrollin')) {
-            slideInFromRight.call(social);
-        } else {
-            social.one('scrollin', slideInFromRight);
-        }
-    }
-
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
-            if (target.length) {
-                $('html,body').animate({scrollTop: target.offset().top}, 1000);
-                return false;
-            }
-        }
-    });
-
-    $('#gotrl-panel-five-carousel').slick({
-        infinite: true,
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        pauseOnHover: false
-    });
-});
-
-function slideInFromRight() {
-    $(this).animate({
-        opacity: 1,
-        marginLeft: 0
-    }, 500);
-}
-
-function slideInFromLeft() {
-    $(this).animate({
-        opacity: 1,
-        marginLeft: -40
-    }, 500);
-}
+// $(function() {
+//     if ($(window).width() > 800) {
+//         var video = $('.gotrl-panel-three-right video').scrolling({
+//             offsetLeft: 700,
+//             offsetTop: -100
+//         });
+//
+//         var anltcs = $('.gotrl-panel-four-left img').scrolling({
+//             offsetLeft: -700,
+//             offsetTop: -30
+//         });
+//
+//         var social = $('.gotrl-panel-social-right img').scrolling({
+//             offsetLeft: 700,
+//             offsetTop: -100
+//         });
+//
+//
+//         if (video.is(':scrollin')) {
+//             slideInFromRight.call(video);
+//         } else {
+//             video.one('scrollin', slideInFromRight);
+//         }
+//
+//         if (anltcs.is(':scrollin')) {
+//             slideInFromLeft.call(anltcs);
+//         } else {
+//             anltcs.one('scrollin', slideInFromLeft);
+//         }
+//
+//         if (social.is(':scrollin')) {
+//             slideInFromRight.call(social);
+//         } else {
+//             social.one('scrollin', slideInFromRight);
+//         }
+//     }
+//
+//     $('a[href*=#]:not([href=#])').click(function() {
+//         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//             var target = $(this.hash);
+//             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//
+//             if (target.length) {
+//                 $('html,body').animate({scrollTop: target.offset().top}, 1000);
+//                 return false;
+//             }
+//         }
+//     });
+//
+//     $('#gotrl-panel-five-carousel').slick({
+//         infinite: true,
+//         dots: true,
+//         arrows: false,
+//         autoplay: true,
+//         autoplaySpeed: 3000,
+//         pauseOnHover: false
+//     });
+// });
+//
+// function slideInFromRight() {
+//     $(this).animate({
+//         opacity: 1,
+//         marginLeft: 0
+//     }, 500);
+// }
+//
+// function slideInFromLeft() {
+//     $(this).animate({
+//         opacity: 1,
+//         marginLeft: -40
+//     }, 500);
+// }
